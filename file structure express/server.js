3c03faea113f0f9664e741c229a0e112050bf17b
souvikrnd <headoffice.test1@seple.in>
2025-01-30 09:10:36 +0530

    Add files via upload

---
+++ file structure express/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require('express');
+const first = require('./routes/first');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const PORT = process.env.PORT
+
+
+
+const app = express();
+app.use(bodyParser.json())
+
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
+app.use('/',first);
+
+app.listen(PORT);
