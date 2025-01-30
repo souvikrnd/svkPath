7e3972244f15c05b941ed707d15eacca007ca997
souvikrnd <headoffice.test1@seple.in>
2025-01-30 17:40:33 +0530

    Delete file structure express directory

--- file structure express/models/first.js
+++
@@ -1,9 +0,0 @@
-const mongo = require('mongoose');
-
-const firstSchema = new mongo.Schema({
-    name:{type:String}
-});
-
-const First = mongo.model("First", firstSchema);
-
-module.exports = First;
