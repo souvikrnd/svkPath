e801f96bbe27ab571cebef3ac5633e3fbf0fbd77
souvikrnd <headoffice.test1@seple.in>
2025-01-30 11:10:50 +0530

    Update server.js

--- file structure express/server.js
+++ file structure express/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const first = require('./routes/first');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 
 
